b4a4beed14108f0827cf56e5edd2a280ca95e630
Caitlin Teague Doerr <74571280+caitlinteaguedesign@users.noreply.github.com>
2021-04-01 15:30:37 -0500

    touch and safari fixes

--- src/js/initializeNav.js
+++ src/js/initializeNav.js
@@ -1,7 +1,10 @@
 function initlializeNav() {
+
    $('#js_menu-button').show().on('click', function(){
       toggleNav();
    });
+
+   $('#js_nav-divider').show();
 
    $('#js_nav').addClass('nav--closed');
 }
